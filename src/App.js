524ddbaab76fecb7040037535970d21e36c11ce6
John Youn <john@Johns-MacBook-Pro.local>
2020-09-11 10:09:41 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Character from "./components/Character";
+import axios from "axios";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [chars, setChars] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get("https://swapi.py4e.com/api/people/")
+      .then((res) => {
+        console.log(res.data.results);
+        setChars(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1>Characters</h1>
+      <Character chars={chars} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,34 @@
-// Write your Character component here
+import React from "react"; // Write your Character component here
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  background-color: ${(pr) => pr.theme.black};
+  color: ${(pr) => pr.theme.white};
+  padding: ${(pr) => pr.theme.paddingSize};
+  margin: ${(pr) => pr.theme.marginSize} auto;
+  opacity: 50%;
+  width: 80%;
+  border-radius: 10px;
+  h2 {
+    color: ${(pr) => pr.theme.blue};
+    font-style: italic;
+  }
+`;
+
+const Character = ({ chars }) => {
+  console.log(chars);
+
+  return (
+    <>
+      {chars.map((char, index) => (
+        <StyledDiv key={index}>
+          <h2>{char.name}</h2>
+          <p>Gender: {char.gender}</p>
+          <p>Height: {char.height}</p>
+          <p>Mass: {char.mass}</p>
+        </StyledDiv>
+      ))}
+    </>
+  );
+};
+export default Character;
